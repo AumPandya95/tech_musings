7c546d2074280162df0d415be05fb9b329f65a0e
Aum Pandya <aumpandya.95@gmail.com>
2021-11-05 21:33:50 +0530

    Update: Check funcion for validation on arrays

--- bit_even_arrays/bit_even_arrays.py
+++ bit_even_arrays/bit_even_arrays.py
@@ -1,3 +1,22 @@
+def check_binary(num):
+    binary_num = []
+    # Convert num to binary
+    print(binary_num)
+    for i, ele in enumerate(binary_num[::-1]):
+        # print('len', len(binary_num))
+        if i >= len(binary_num) - 1:
+            break
+        elif (i+1)%2 == 1:
+            if ele != 0:
+                # print(f"odd -> {ele}")
+                num = check_binary(num+2)
+                return num
+            else:
+                pass
+                # print(f"odd -> {ele}")
+    return num
+
+
 def replacer(arr):
     even_gen = 0
     positions_filled = []
@@ -10,9 +29,9 @@ def replacer(arr):
             else:
                 even_gen = 2 * ((arr[i - 1] // 2) + 1)
             
-            # Replace here
+            replace_with_num = check_binary(even_gen)
             arr.pop(i)
-            arr.insert(i, 0)  # replace_with_num
+            arr.insert(i, replace_with_num)
     
     return positions_filled
 
